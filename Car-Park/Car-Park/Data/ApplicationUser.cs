751a291f265c2faff7d5faa039d56b346316875b
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2025-03-23 12:46:54 +0200

    Models

---
+++ Car-Park/Car-Park/Data/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Car_Park.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Role { get; set; } 
+    }
+}
---
+++ Car-Park/Car-Park/Data/Trip.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Car_Park.Data
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Route { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime ExpectedReturnTime { get; set; }
+
+        public DateTime? ActualReturnTime { get; set; }
+
+        [Required]
+        public string Status { get; set; }  // "Pending", "InProgress", "Canceled", "Completed"
+
+        public string DriverId { get; set; }
+        public ApplicationUser Driver { get; set; } 
+    }
+}
